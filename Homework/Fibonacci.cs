8c124737c50c6744b097f418f768a95e376973b4
Ben <turnerbenjamin25@gmail.com>
2023-03-06 13:07:25 -0500

    The old college try

--- Homework/Fibonacci.cs
+++ Homework/Fibonacci.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,36 @@ namespace Homework
 {
     public class Fibonacci
     {
+        private Retrieve(fibonacciIndex);
         // this method takes an index as a parameter and returns the fibonacci number at that index.
         public long Retrieve(int fibonacciIndex)
         {
-            return 0;
+            if (fibonacciIndex <= 1)
+            {
+                return fibonacciIndex;
+            }
+            else
+            {
+                return Retrieve(fibonacciIndex - 1) + Retrieve(fibonacciIndex - 2);
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
